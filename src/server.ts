3b9305b0695dd898e3fe799c2f58d0c8cd730b85
Benjamín <benser22@hotmail.com>
2024-07-22 13:59:51 -0300

    initial setup

--- src/server.ts
+++ src/server.ts
@@ -12,53 +12,40 @@
 //   }
 // }
 
-// src/server.ts
-import express from 'express'
-import mongoose from 'mongoose'
-import router from './routes'
+import express from 'express';
+import connectToDatabase from './config/database';
+import router from './routes';
 
 export class Server {
-  public app: express.Application
-  private port: string | number
-  private mongoUri: string
+  public app: express.Application;
+  private port: string | number;
 
   constructor() {
-    this.app = express()
-    this.port = process.env.PORT || 3000
-    this.mongoUri =
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase'
+    this.app = express();
+    this.port = process.env.PORT || 3000;
 
-    this.middlewares()
-    this.routes()
-    this.databaseConnection()
+    this.middlewares();
+    this.routes();
+    this.databaseConnection();
   }
 
   middlewares() {
-    this.app.use(express.json())
+    this.app.use(express.json());
   }
 
   routes() {
-    this.app.use('/api', router)
+    this.app.use('/api', router);
   }
 
   async databaseConnection() {
-    try {
-      await mongoose.connect(this.mongoUri, {
-        // Las opciones de conexión antiguas se han eliminado
-        // El nuevo conector no requiere las opciones `useNewUrlParser` ni `useUnifiedTopology`
-        // No obstante, puedes agregar opciones como `autoIndex` si es necesario
-      })
-      console.log('Database connected successfully')
-    } catch (error) {
-      console.error('Database connection error:', error)
-    }
+    await connectToDatabase();
   }
 
   listen() {
     this.app.listen(this.port, () => {
-      console.log(`Server running on port ${this.port}`)
-    })
+      console.log(`Server running on port ${this.port}`);
+    });
   }
 }
 
-export default Server
+export default Server;
